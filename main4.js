5cc0ee68d4af773b8274dc58a393ba9cf7f35df4
mahnoor77 <mahnoorwattoo77@gmail.com>
2024-09-15 07:57:29 +0500

    First commit

---
+++ main4.js
@@ -0,0 +1,10 @@
+function toggleDropdown(id){
+   var dropdownMenu= document.getElementById(id);
+   if (dropdownMenu.style.display=="none"){
+    dropdownMenu.style.display="block";
+   }
+   else{
+    dropdownMenu.style.display="none";
+   }
+
+}
